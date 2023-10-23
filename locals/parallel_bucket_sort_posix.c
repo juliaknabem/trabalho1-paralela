9d03235c7831afbda55104b47a5afc879807deb3
Diego Goiana <86124730+diegovsg@users.noreply.github.com>
2023-10-22 21:45:07 -0300

    Update parallel_bucket_sort_posix.c

--- locals/parallel_bucket_sort_posix.c
+++ locals/parallel_bucket_sort_posix.c
@@ -1,135 +1,198 @@
-#include <omp.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <time.h>
+#include "randomlist.h"
 
-#define max_threads 80
+//#define NARRAY 110000  // Array size
+#define NBUCKET 1000  // Number of buckets
+#define INTERVAL 10001  // Each bucket capacity
+#define NUM_THREADS 4
 
-struct bucket {
-    int n_elem;
-    int index; // [start : n_elem)
-    int start; //starting point in B array
+struct Node
+{
+    int data;
+    struct Node *next;
 };
 
-int cmpfunc (const void * a, const void * b)
+struct ThreadData
 {
- return ( *(int*)a - *(int*)b );
-}
+    int *arr;
+    struct Node **buckets;
+    int start;
+    int end;
+};
 
-int main(int argc, char *argv[]) {
+void BucketSort(int arr[]);
+struct Node *InsertionSort(struct Node *list);
+void print(int arr[]);
+int getBucketIndex(int value);
 
-    int *A, *B, *temp;
-    int dim, n_buckets, i, w, limit, num_threads, workload, b_index;
-    struct bucket* buckets; //array of buckets
-    double t1; // Timing variable
-    float total; //total time
-    n_buckets = 5;
-	  int *tmp;
-    dim = 10000;
-    limit = 100000;
+void *ParallelBucketSort(void *data);
 
-    //global buckets
-    int global_n_elem[n_buckets]; //number of elements in each bucket
-    int global_starting_position[n_buckets]; //starting position in A for each bucket
-    memset(global_n_elem, 0, sizeof(int)*n_buckets);
-    memset(global_starting_position, 0, sizeof(int)*n_buckets);
+pthread_mutex_t mutex;
 
-    num_threads = n_buckets;
-    printf("number of threads %d", &num_threads);
-    omp_set_num_threads(num_threads);
-
-    A = (int *) malloc(sizeof(int)*dim);
-    B = (int *) malloc(sizeof(int)*dim);
-    srand(time(NULL));
-    for(i=0;i<dim;i++) {
-	    A[i] = rand() % limit;
-	}
-
-
-    //local buckets, n_buckets for each thread
-    buckets = (struct bucket *) calloc(n_buckets*num_threads, sizeof(struct bucket));
-    t1 = omp_get_wtime();
-
-#pragma omp parallel
+void BucketSort(int arr[])
 {
-    num_threads = omp_get_num_threads();
+    int i;
+    struct Node **buckets = (struct Node **)malloc(sizeof(struct Node *) * NBUCKET);
 
-    int j,k;
-    int local_index; // [0 : n_buckets)
-    int real_bucket_index; // [0 : n_buckets * num_threads)
-    int my_id = omp_get_thread_num();
-    workload = dim/num_threads;
-	int prevoius_index;
-
-   #pragma omp for private(i,local_index)
-    for (i=0; i< dim;i++){
-        local_index = A[i]/w;
-        if (local_index > n_buckets-1)
-                local_index = n_buckets-1;
-        real_bucket_index = local_index + my_id*n_buckets;
-        buckets[real_bucket_index].n_elem++;
-	}   
-   
-	int local_sum=0;
-	for (j=my_id; j< n_buckets*num_threads; j=j+num_threads){
-		local_sum += buckets[j].n_elem;
-	}
-	global_n_elem[my_id]=local_sum;
-	
-	#pragma omp barrier
-	
-	#pragma omp master
+    for (i = 0; i < NBUCKET; ++i)
     {
-	 for (j=1; j<n_buckets; j++){
-		        global_starting_position[j] = global_starting_position[j-1] + global_n_elem[j-1];	 
-				buckets[j].start = buckets[j-1].start + global_n_elem[j-1];
-				buckets[j].index = buckets[j-1].index + global_n_elem[j-1];
-		}
-	}
-
-	#pragma omp barrier
-	for (j=my_id+n_buckets; j< n_buckets*num_threads; j=j+num_threads){
-		int prevoius_index = j-n_buckets;
-		buckets[j].start = buckets[prevoius_index].start + buckets[prevoius_index].n_elem;
-		buckets[j].index = buckets[prevoius_index].index + buckets[prevoius_index].n_elem;	
-	}
-	#pragma omp barrier
-
-    #pragma omp for private(i, b_index)
-    for (i=0; i< dim ;i++){
-        j = A[i]/w;
-        if (j > n_buckets -1)
-                j = n_buckets-1;
-        k = j + my_id*n_buckets;
-        b_index = buckets[k].index++;
-        B[b_index] = A[i];
+        buckets[i] = NULL;
     }
 
-#pragma omp for private(i)
-    for(i=0; i<n_buckets; i++)
-        qsort(B+global_starting_position[i], global_n_elem[i], sizeof(int), cmpfunc);
-}
-   total = omp_get_wtime() - t1;
-	tmp = A;
-	A = B;
-	B = tmp;
+    for (i = 0; i < dim; ++i)
+    {
+        struct Node *current;
+        int pos = getBucketIndex(arr[i]);
+        current = (struct Node *)malloc(sizeof(struct Node));
+        current->data = arr[i];
+        current->next = buckets[pos];
+        buckets[pos] = current;
+    }
 
-   if (dim <= 40) {
-        printf("A \n");
-        for(i=0;i<dim;i++) {
-            printf("%d ",A[i]);
+    pthread_t threads[NUM_THREADS];
+    struct ThreadData threadData[NUM_THREADS];
+
+    pthread_mutex_init(&mutex, NULL);
+
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
+        threadData[i].arr = arr;
+        threadData[i].buckets = buckets;
+        threadData[i].start = i * (NBUCKET / NUM_THREADS);
+        threadData[i].end = (i + 1) * (NBUCKET / NUM_THREADS);
+
+        pthread_create(&threads[i], NULL, ParallelBucketSort, &threadData[i]);
+    }
+
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+
+    int j = 0;
+    for (i = 0; i < NBUCKET; ++i)
+    {
+        struct Node *node = buckets[i];
+        while (node)
+        {
+            arr[j++] = node->data;
+            struct Node *temp = node;
+            node = node->next;
+            free(temp);
         }
-        printf("\n");
     }
-    printf("Sorting %d elements took %f seconds\n", dim,total);
 
-    int sorted = 1;
-    for(i=0;i<dim-1;i++) {
-        if (A[i] > A[i+1])
-            sorted = 0;
-    }
-    if (!sorted)
-        printf("The data is not sorted!!!\n");
+    free(buckets);
 }
 
+void *ParallelBucketSort(void *data)
+{
+    struct ThreadData *threadData = (struct ThreadData *)data;
+    int *arr = threadData->arr;
+    struct Node **buckets = threadData->buckets;
+    int start = threadData->start;
+    int end = threadData->end;
+
+    for (int i = start; i < end; ++i)
+    {
+        buckets[i] = InsertionSort(buckets[i]);
+    }
+
+    return NULL;
+}
+
+struct Node *InsertionSort(struct Node *list)
+{
+    struct Node *nodeList = list;
+    if (list == NULL || list->next == NULL)
+    {
+        return list;
+    }
+
+    struct Node *k = list->next;
+    nodeList->next = NULL;
+
+    while (k != NULL)
+    {
+        struct Node *ptr;
+        if (nodeList->data > k->data)
+        {
+            struct Node *tmp = k;
+            k = k->next;
+            tmp->next = nodeList;
+            nodeList = tmp;
+            continue;
+        }
+
+        for (ptr = nodeList; ptr->next != NULL; ptr = ptr->next)
+        {
+            if (ptr->next->data > k->data)
+                break;
+        }
+
+        if (ptr->next != NULL)
+        {
+            struct Node *tmp = k;
+            k = k->next;
+            tmp->next = ptr->next;
+            ptr->next = tmp;
+            continue;
+        }
+        else
+        {
+            ptr->next = k;
+            k = k->next;
+            ptr->next->next = NULL;
+            continue;
+        }
+    }
+
+    return nodeList;
+}
+
+int getBucketIndex(int value)
+{
+    return value / INTERVAL;
+}
+
+void print(int arr[])
+{
+    int i;
+    for (i = 0; i < dim; ++i)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    //int array[NARRAY];
+    initializeRandomList(100000);
+
+    /*for (int i = 0; i < NARRAY; i++)
+    {
+        array[i] = rand() % NARRAY + 1; // Gera um número aleatório
+    }*/
+
+    clock_t t0, t1;
+    t0 = clock();
+
+    BucketSort(A);
+
+    t1 = clock();
+    double elapsed_time = (double)(t1 - t0) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", elapsed_time);
+
+    // Imprimir os resultados ordenados
+    //printf("Sorted array: ");
+    //print(array);
+
+
+    return 0;
+}
