85c83d166b4daa0b3ab46827378d734e3380f919
Julia Fernanda Werlang <104525139+juufernandaw@users.noreply.github.com>
2023-10-29 19:30:01 -0300

    Update randomlist.c

--- locals/randomlist.c
+++ locals/randomlist.c
@@ -5,12 +5,31 @@ int *A = NULL;
 int dim = 0;
 
 void initializeRandomList(int n) {
+    srand(time(NULL));
     A = (int *)malloc(sizeof(int) * n);
     dim = n;
     for (int i = 0; i < n; i++) {
-        A[i] = rand(); // Preenche a lista com números aleatórios
+        A[i] = rand() % n + 1; // Preenche a lista com números aleatórios
     }
 }
+
+// Números mal distribuídos:
+// void initializeRandomList(int n) {
+//     srand(time(NULL));
+//     A = (int *)malloc(sizeof(int) * n);
+//     dim = n;
+//     int range = n / 2; // Define o tamanho da "zona de concentração" nas extremidades
+
+//     for (int i = 0; i < n; i++) {
+//         if (i < range || i >= n - range) {
+//             // Gere números próximos às extremidades com uma probabilidade maior
+//             A[i] = rand() % range;
+//         } else {
+//             // Gere números no centro com uma probabilidade menor
+//             A[i] = range + rand() % (n - 2 * range);
+//         }
+//     }
+// }
 
 void freeRandomList() {
     free(A);
